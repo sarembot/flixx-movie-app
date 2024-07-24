07c7520d270071c4cd3e3ef7f68e8d5612767a28
sarembot <sarembot@github.com>
2024-07-23 17:25:13 -0700

    fixed html linking issues and created a simple router for different pages.

--- js/script.js
+++ js/script.js
@@ -4,13 +4,41 @@ const globalState = {
 
 console.log(globalState.currentPage);
 
+// Highlight active link
+function highlightLink() {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach((link) => {
+    console.log(link.getAttribute('href'));
+
+    if (link.getAttribute('href') === `.${globalState.currentPage}`) {
+      link.classList.toggle('active');
+    }
+  });
+}
+
 // Init App
 function init() {
   switch (globalState.currentPage) {
     case '/':
+    case '/index.html':
       console.log('home');
       break;
+    case '/shows.html':
+      console.log('shows');
+      break;
+    case '/movie-details.html':
+      console.log('movie details');
+      break;
+    case '/tv-details.html':
+      console.log('tv details');
+      break;
+    case '/search.html':
+      console.log('search');
+      break;
   }
+
+  highlightLink();
 }
 
 document.addEventListener('DOMContentLoaded', init);
