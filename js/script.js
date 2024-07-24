e43326f05b357be19a38d1562fe96069a1b235b5
sarembot <sarembot@github.com>
2024-07-24 11:39:10 -0700

    fetched popular movie data and added it to home page.

--- js/script.js
+++ js/script.js
@@ -9,11 +9,56 @@ function highlightLink() {
   const navLinks = document.querySelectorAll('.nav-link');
 
   navLinks.forEach((link) => {
-    console.log(link.getAttribute('href'));
-
     if (link.getAttribute('href') === `.${globalState.currentPage}`) {
       link.classList.toggle('active');
     }
+  });
+}
+
+// make fetch request to moviedb, get popular movies, display them on the page
+
+async function fetchApiData(endpoint) {
+  const API_KEY = '6969dc387a25455aada3bf89c5cbed31';
+  const API_URL = 'https://api.themoviedb.org/3/';
+
+  const res = await fetch(
+    `${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`
+  );
+  const data = await res.json();
+
+  return data;
+}
+
+// Display popular movies
+async function displayPopularMovies() {
+  const popularMovies = await fetchApiData('movie/popular');
+
+  console.log(popularMovies.results);
+
+  popularMovies.results.forEach((movie) => {
+    const container = document.getElementById('popular-movies');
+    const movieDiv = document.createElement('div');
+    movieDiv.innerHTML = `<div class="card">
+          <a href="movie-details.html?id=1">
+            <img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+            />
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>`;
+
+    container.appendChild(movieDiv);
+    const movieTitle = document.querySelector('.card-title');
+    const releaseDate = document.querySelector('.text-muted');
+
+    movieTitle.innerText = movie.title;
+    releaseDate.innerText = movie.release_date;
   });
 }
 
@@ -39,6 +84,7 @@ function init() {
   }
 
   highlightLink();
+  displayPopularMovies();
 }
 
 document.addEventListener('DOMContentLoaded', init);
